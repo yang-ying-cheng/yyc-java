1eab0c10eb466a45afd0ab58941ffb46c373fd40
杨英承 <y2689615085@gmail.com>
2021-09-24 16:45:08 +0800

    测试wait()和notify()

---
+++ src/Thread/WaitOrNotifyThread.java
@@ -0,0 +1,70 @@
+package Thread;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.concurrent.TimeUnit;
+
+public class WaitOrNotifyThread {
+    static boolean flag = true;
+    static Object lock = new Object();
+    public static void main(String[] args) throws InterruptedException {
+        Thread waitThread = new Thread(new WaitThread(),"waitThread");
+        waitThread.start();
+        TimeUnit.SECONDS.sleep(1);
+        Thread notifyThread = new Thread(new NotifyThread(),"notifyThread");
+        notifyThread.start();
+    }
+    static class WaitThread implements Runnable{
+
+        @Override
+        public void run() {
+            // 加锁，拥有lock的Monitor
+            synchronized (lock){
+                while (flag){
+                    try {
+                        System.out.println(Thread.currentThread() + " flag is true. wait @ " + LocalDateTime.now().format(DateTimeFormatter.ofPattern("HH:mm:ss")));
+                        lock.wait();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+                System.out.println(Thread.currentThread() + " flag is false. running @ " + LocalDateTime.now().format(DateTimeFormatter.ofPattern("HH:mm:ss")));
+            }
+        }
+    }
+
+    static class NotifyThread implements Runnable{
+
+        @Override
+        public void run() {
+            synchronized (lock){
+                // 获取锁，然后进行通知，通知不会释放lock的锁
+                // 直到当前线程释放了lock后，waitThread才能从wait方法中返回
+                System.out.println(Thread.currentThread() + " hold lock. notify @ " + LocalDateTime.now().format(DateTimeFormatter.ofPattern("HH:mm:ss")));
+                lock.notifyAll();
+                flag = false;
+                // 睡眠5秒
+                try {
+                    SleepUtils.second(5);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+//            try {
+//                SleepUtils.second(1);
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
+            // 再次加锁
+            synchronized (lock){
+                System.out.println(Thread.currentThread() + " hold lock again. sleep @ " + LocalDateTime.now().format(DateTimeFormatter.ofPattern("HH:mm:ss")));
+                try {
+                    SleepUtils.second(5);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+}
