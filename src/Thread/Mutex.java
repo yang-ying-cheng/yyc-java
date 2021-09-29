b619a289b9a4a0f5926d0f4ca64b863cb1f021ac
杨英承 <y2689615085@gmail.com>
2021-09-29 17:43:41 +0800

    添加独占锁（不可重入锁）

--- src/Thread/Mutex.java
+++ src/Thread/Mutex.java
@@ -17,19 +17,21 @@ public class Mutex implements Lock {
         }
         // 当状态为0的时候获取锁
         public boolean tryAcquire(int acquires){
+            // 0表示锁定状态，1表示锁定状态
             if(compareAndSetState(0,1)){
                 setExclusiveOwnerThread(Thread.currentThread());
                 return true;
             }
             return false;
         }
-        // 释放锁，将状态设置为 0
+        // 释放锁，将状态设置为 0，试图设置状态来反映独占模式下的一个释放。
         protected boolean tryRelease(int release){
             if(getState() == 0){
                 throw new IllegalMonitorStateException();
             }
             // 设置当前拥有独占访问权限的线程。 null 参数表示没有线程拥有访问权限
             setExclusiveOwnerThread(null);
+            // 设置同步状态的值。
             setState(0);
             return true;
         }
@@ -43,39 +45,47 @@ public class Mutex implements Lock {
 
     @Override
     public void lock(){
+        // 以独占模式获取对象，忽略中断
         sync.acquire(1);
     }
 
     @Override
     public boolean tryLock() {
+        // 试图在独占模式下获取对象状态。
         return sync.tryAcquire(1);
     }
 
     @Override
     public boolean tryLock(long timeout, TimeUnit unit) throws InterruptedException {
+        // 试图以独占模式获取对象，如果被中断则中止，如果到了给定超时时间，则会失败。
         return sync.tryAcquireNanos(1,unit.toNanos(timeout));
     }
 
     @Override
     public void unlock() {
+        //  以独占模式释放对象。
         sync.release(1);
     }
 
     @Override
     public Condition newCondition() {
+
         return sync.newCondition();
     }
 
     public boolean isLocked(){
+        // 判断同步对于当前线程是否是独立的
         return sync.isHeldExclusively();
     }
 
     public boolean hasQueuedThreads(){
+        // 查询是否有正在等待获取的任何线程。
         return sync.hasQueuedThreads();
     }
 
     @Override
     public void lockInterruptibly() throws InterruptedException {
+        // 以独占模式获取对象，如果被中断则中止。
         sync.acquireInterruptibly(1);
     }
 
