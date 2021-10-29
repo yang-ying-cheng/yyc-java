ef524684a4c54276f0bff99d292c70f906f303cc
杨英承 <y2689615085@gmail.com>
2021-10-29 17:01:58 +0800

    添加读写锁使用案例

---
+++ src/Thread/Cache.java
@@ -0,0 +1,56 @@
+package Thread;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
+public class Cache {
+
+    static Map<String, Object> map = new HashMap<>();
+    static ReentrantReadWriteLock lock = new ReentrantReadWriteLock();
+    // 读锁
+    static Lock readLock = lock.readLock();
+    // 写锁
+    static Lock writeLock = lock.writeLock();
+
+    /**
+     * 获取key对应的value
+     *
+     * @param key
+     * @return
+     */
+    public static final Object getKey(String key) {
+        try {
+            readLock.lock();
+            return map.get(key);
+        } finally {
+            readLock.unlock();
+        }
+    }
+
+    /**
+     * 放进缓存中
+     * @param key key
+     * @param object value
+     * @return
+     */
+    public static Object put(String key, Object object) {
+
+        try {
+            writeLock.lock();
+            return map.put(key, object);
+        } finally {
+            writeLock.unlock();
+        }
+    }
+
+    public static void clear() {
+        try {
+            writeLock.lock();
+            map.clear();
+        } finally {
+            writeLock.unlock();
+        }
+    }
+}
---
+++ src/basicJava/ByteTest.java
@@ -0,0 +1,8 @@
+package basicJava;
+
+public class ByteTest {
+    public static void main(String[] args) {
+        System.out.println(0x0000FFFF);
+//        System.out.println(1<<16);
+    }
+}
